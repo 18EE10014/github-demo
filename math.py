afd660290ced530e7cc47f8bf20d9eabf6e37116
18EE10014 <arnavarnav783@gmail.com>
2021-06-16 15:34:18 +0530

    Mul implementation

--- math.py
+++ math.py
@@ -3,10 +3,10 @@ def add(x,y):
     return (x+y)
 #Subtarcting implementation
 def subtract(x,y):
-    return x-y       # on master branch
+    return x-y       # on main branch
 #Multiplying implementation
 def multiply(x,y):
-    return x*y        # on Bug456 branch
+    return (x*y)        # on Bug456 branch
 #Dividing implementation
 def divide(x,y):
     if x<0:
